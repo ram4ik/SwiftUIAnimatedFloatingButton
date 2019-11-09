85d11fefd6ac0792a660fbb75e00383c437f9a35
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-11-09 17:28:54 +0200

    Initial Commit

---
+++ SwiftUIAnimatedFloatingButton/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAnimatedFloatingButton
+//
+//  Created by Ramill Ibragimov on 09.11.2019.
+//  Copyright © 2019 Ramill Ibragimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
